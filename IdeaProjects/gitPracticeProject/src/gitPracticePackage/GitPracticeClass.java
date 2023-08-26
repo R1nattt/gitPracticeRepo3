3c5df11a5cc3c082cf5c58c061abbb368faddf00
learn github <rinat.akrem@learngihub.com>
2023-08-26 14:37:58 -0400

    Adding gitPracticeProject to local repo

---
+++ IdeaProjects/gitPracticeProject/src/gitPracticePackage/GitPracticeClass.java
@@ -0,0 +1,10 @@
+package gitPracticePackage;
+
+public class GitPracticeClass {
+    public static void main(String []args){
+        System.out.println("Creating a practice project to git familiar with git and github");
+
+
+
+    }
+}
