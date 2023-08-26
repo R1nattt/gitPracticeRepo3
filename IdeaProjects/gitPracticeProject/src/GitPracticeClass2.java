04748c209a139d22b8f0d2e6ead2effa8bdbf63d
learn github <rinat.akrem@learngihub.com>
2023-08-26 15:55:58 -0400

    Adding GitPractice2 class

---
+++ IdeaProjects/gitPracticeProject/src/GitPracticeClass2.java
@@ -0,0 +1,6 @@
+public class GitPracticeClass2 {
+    public static void main(String[] args) {
+        System.out.println("Practicing a my own branch called demoBranch1" +
+                "");
+    }
+}
